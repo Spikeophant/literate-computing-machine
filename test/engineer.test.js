895a5cad4f206e215b7a8201fbda17c269c07fa0
Dustin Davisson <dustin.davisson@gmail.com>
2022-09-10 11:07:39 -0700

    Add engineer and intern tests.

---
+++ test/engineer.test.js
@@ -0,0 +1,10 @@
+const Engineer = require('../lib/engineer');
+describe('Engineer', () => {
+  it('Create a engineer with a given Name, Email and employee ID, github username.', () => {
+    const engineer = new Engineer('Engineer1', '1', 'engineer1@company.com', 'engineer1');
+    expect(engineer.name).toEqual('Engineer1');
+    expect(engineer.employeeID).toEqual('1');
+    expect(engineer.email).toEqual('engineer1@company.com');
+    expect(engineer.github).toEqual('engineer1');
+  })
+})
---
+++ test/intern.test.js
@@ -0,0 +1,10 @@
+const Intern = require('../lib/intern');
+describe('Intern', () => {
+  it('Create a intern with a given Name, Email and employee ID, school.', () => {
+    const intern = new Intern('Intern1', '2', 'intern1@company.com', 'school1');
+    expect(intern.name).toEqual('Intern1');
+    expect(intern.employeeID).toEqual('2');
+    expect(intern.email).toEqual('intern1@company.com');
+    expect(intern.school).toEqual('school1');
+  })
+})
