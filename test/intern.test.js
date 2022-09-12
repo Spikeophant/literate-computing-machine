0293a6b7f296b347e0e368e26b2306c93f3c570e
Dustin Davisson <dustin.davisson@gmail.com>
2022-09-11 21:46:48 -0700

    Change classes to use employee super class.

--- test/intern.test.js
+++ test/intern.test.js
@@ -3,9 +3,10 @@ describe('Intern', () => {
   it('Create a intern with a given Name, Email and employee ID, school.', () => {
     const intern = new Intern('Intern1', '2', 'intern1@company.com', 'school1');
     expect(intern.name).toEqual('Intern1');
-    expect(intern.employeeID).toEqual('2');
+    expect(intern.id).toEqual('2');
     expect(intern.email).toEqual('intern1@company.com');
     expect(intern.school).toEqual('school1');
     expect(intern.role).toEqual('intern');
+    expect(intern.getSchool()).toEqual(intern.school)
   })
 })
