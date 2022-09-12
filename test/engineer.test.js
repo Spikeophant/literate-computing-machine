01caa21dbae71ee877d2ba1e6489d5234c0002c8
Dustin Davisson <dustin.davisson@gmail.com>
2022-09-11 21:15:52 -0700

    Change classes to use employee super class.

--- test/engineer.test.js
+++ test/engineer.test.js
@@ -3,9 +3,10 @@ describe('Engineer', () => {
   it('Create a engineer with a given Name, Email and employee ID, github username.', () => {
     const engineer = new Engineer('Engineer1', '1', 'engineer1@company.com', 'engineer1');
     expect(engineer.name).toEqual('Engineer1');
-    expect(engineer.employeeID).toEqual('1');
+    expect(engineer.id).toEqual('1');
     expect(engineer.email).toEqual('engineer1@company.com');
     expect(engineer.github).toEqual('engineer1');
     expect(engineer.role).toEqual('engineer');
+    expect(engineer.getGithub()).toEqual(engineer.github);
   })
 })
