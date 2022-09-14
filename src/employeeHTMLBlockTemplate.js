e9d1081d555252e487051a1ffcef30d543717726
Dustin Davisson <dustin.davisson@gmail.com>
2022-09-14 14:29:28 -0700

    Fix typo on link in employeeHTMLBlockTemplate.js

--- src/employeeHTMLBlockTemplate.js
+++ src/employeeHTMLBlockTemplate.js
@@ -1,7 +1,7 @@
 const managerHTML = data => `<!-- Team item-->
         <div class="col-xl-3 col-sm-6 mb-5">
             <div class="bg-white rounded shadow-sm py-5 px-4"><img src="https://bootdey.com/img/Content/avatar/avatar2.png" alt="" width="100" class="img-fluid rounded-circle mb-3 img-thumbnail shadow-sm">
-                <h5 class="mb-0"><a href="mailto:${data.getEmail()}>"${data.getName()}</a></h5><span class="small text-uppercase text-muted">${data.getRole()}</span>
+                <h5 class="mb-0"><a href="mailto:${data.getEmail()}">${data.getName()}</a></h5><span class="small text-uppercase text-muted">${data.getRole()}</span>
                 <ul class="social mb-0 list-inline mt-3">
                     <li class="list-inline-item">office: ${data.getOffice()}</li>
                 </ul>
@@ -12,7 +12,7 @@ const managerHTML = data => `<!-- Team item-->
 const engineerHTML = data => `<!-- Team item-->
         <div class="col-xl-3 col-sm-6 mb-5">
             <div class="bg-white rounded shadow-sm py-5 px-4"><img src="https://bootdey.com/img/Content/avatar/avatar2.png" alt="" width="100" class="img-fluid rounded-circle mb-3 img-thumbnail shadow-sm">
-                <h5 class="mb-0"><a href="mailto:${data.getEmail()}>"${data.getName()}</a></h5><span class="small text-uppercase text-muted">${data.getRole()}</span>
+                <h5 class="mb-0"><a href="mailto:${data.getEmail()}">${data.getName()}</a></h5><span class="small text-uppercase text-muted">${data.getRole()}</span>
                 <ul class="social mb-0 list-inline mt-3">
                     <li class="list-inline-item"><a href="https://github.com/${data.getGithub}" class="social-link"><i class="fa fa-github"></i></a></li>
                 </ul>
@@ -23,7 +23,7 @@ const engineerHTML = data => `<!-- Team item-->
 const internHTML = data => `<!-- Team item-->
         <div class="col-xl-3 col-sm-6 mb-5">
             <div class="bg-white rounded shadow-sm py-5 px-4"><img src="https://bootdey.com/img/Content/avatar/avatar2.png" alt="" width="100" class="img-fluid rounded-circle mb-3 img-thumbnail shadow-sm">
-                <h5 class="mb-0"><a href="mailto:${data.getEmail()}>"${data.getName()}</a></h5><span class="small text-uppercase text-muted">${data.getRole()}</span>
+                <h5 class="mb-0"><a href="mailto:${data.getEmail()}">${data.getName()}</a></h5><span class="small text-uppercase text-muted">${data.getRole()}</span>
                 <ul class="social mb-0 list-inline mt-3">
                     <li class="list-inline-item">school: ${data.getSchool()}</li>
                 </ul>
