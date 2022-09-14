d8a266865f59a66f6bbf2715f37e8211d4db6745
Dustin Davisson <dustin.davisson@gmail.com>
2022-09-13 21:52:55 -0700

    Inquirer questions and basic application flow.

--- index.js
+++ index.js
@@ -8,4 +8,47 @@ const questions = require('./lib/questions');
 //initialize Team variable to use throughout, and to use
 // to build our resulting HTML.
 let employees = [];
+// start the team build here.  We always start with a manager.
 
+
+const addTeam = () => {
+  return inquirer.prompt(questions.engQuestions)
+    .then(answers => {
+      if (answers.action === 'Quit') {
+        return employees;
+      } else {
+        if (answers.action === 'Intern') {
+          const employee = new Intern(answers.name, answers.id, answers.email, answers.school);
+          employees.push(employee);
+        } else {
+          console.log(answers);
+          const employee = new Engineer(answers.name, answers.id, answers.email, answers.github);
+          employees.push(employee);
+        }
+        return addTeam();
+      }
+    })
+    .catch(err => console.log(err));
+}
+
+const buildTeam = () => {
+  return inquirer.prompt(questions.managerQuestions)
+    .then(answers => {
+      const employee = new Manager(answers.name, answers.id, answers.email, answers.office);
+      employees.push(employee);
+    })
+    .catch(err => console.log(`Something went wrong:\n ${err}`)
+    )
+}
+
+const writeFile = () => {
+  return console.log(employees);
+}
+
+
+buildTeam()
+  .then(addTeam)
+    .then(employees => {
+      return writeFile(employees);
+    })
+  .catch(err => console.log(err));1
